478744f8b11d2cd163a341f297b86af2eacae08b
Moeen Mahmud <write2fahim007@gmail.com>
2021-10-29 11:35:19 +0600

    connected mongo client

--- index.js
+++ index.js
@@ -1,7 +1,31 @@
 const express = require("express");
+const { MongoClient } = require("mongodb");
+require("dotenv").config();
 const cors = require("cors");
 const app = express();
 const port = process.env.PORT || 5000;
+
+//Middleware
+app.use(cors());
+app.use(express.json());
+
+//Mongo Client
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@travelbookingcluster.ew8yc.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+async function run() {
+  try {
+    await client.connect();
+    console.log("Client Connect Successfully!");
+  } finally {
+    // await client.close();
+  }
+}
+
+run().catch(console.dir);
 
 //Getting main endpoint
 app.get("/", (req, res) => {
